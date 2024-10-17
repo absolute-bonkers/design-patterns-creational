bb78d307cda6dfca7bf7883420f5ecbb92b735b5
Aditya Chandeliya <adityachandeliya@gmail.com>
2024-10-18 02:40:19 +0530

    onboard sub-module factory

---
+++ dp-factory/src/test/java/com/absolute/bonkers/factory/temp/TempTestTest.java
@@ -0,0 +1,16 @@
+package com.absolute.bonkers.factory.temp;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TempTestTest {
+
+    @Test
+    void testSum() {
+
+        TempTest sample = TempTest.builder().a(10).b(5).build();
+        int result = TempTest.calculateSum(sample);
+        Assertions.assertThat(result).isEqualTo(15);
+
+    }
+}
